a473ff86f80fb74e8d6ef93c8e2c66a1886fafb7
nanashinogonbee <vgpro80@gmail.com>
2019-02-27 00:36:31 +0300

    Enhanced module behaviour

--- fuckingthing.py
+++ fuckingthing.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import os
 import traceback
 
 if __name__ == '__main__':
@@ -10,6 +11,10 @@ else:
     except Exception as e:
         tb = traceback.format_exc().splitlines()
         module = tb[-1].split()[-1]
+        path = os.path.dirname(sys.argv[0])
+        full_path = f'{os.path.abspath(path)}/{sys.argv[0]}'
+        tb[1] = f'  File "{full_path}", line 9, in <module>'
         tb[2] = f'  import {module}'
         for line in tb:
             print(line)
+        sys.exit()
