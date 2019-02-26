09489dc22d82f8c5030df6bbbde61239abfb37d3
nanashinogonbee <vgpro80@gmail.com>
2019-02-27 01:04:35 +0300

    True import lines simulation

--- fuckingthing.py
+++ fuckingthing.py
@@ -13,7 +13,10 @@ else:
         module = tb[-1].split()[-1]
         path = os.path.dirname(sys.argv[0])
         full_path = f'{os.path.abspath(path)}/{sys.argv[0]}'
-        tb[1] = f'  File "{full_path}", line 9, in <module>'
+        script = open(full_path).readlines()
+        imports = len([i for i in script if i.startswith('import ')])
+        randline = random.randint(1, imports + 1)
+        tb[1] = f'  File "{full_path}", line {randline}, in <module>'
         tb[2] = f'  import {module}'
         for line in tb:
             print(line)
