e7e04886ec1f6c265ff39fb01869805203feb685
nanashinogonbee <vgpro80@gmail.com>
2019-02-27 07:02:17 +0300

    Import system fixed

--- fuckingthing.py
+++ fuckingthing.py
@@ -7,9 +7,13 @@ if __name__ == '__main__':
     print(f'Import this module via "import {sys.argv[0].split(".")[0]}".')
 else:
     try:
-        raise ModuleNotFoundError(random.choice(dir()))
-    except Exception as e:
-        tb = traceback.format_exc().splitlines()
+        path = os.path.dirname(sys.argv[0])
+        full_path = f'{os.path.abspath(path)}/{sys.argv[0]}'
+        script = open(full_path).readlines()
+        imports = [i for i in script if i.startswith('import ')]
+        raise ModuleNotFoundError(random.choice(imports))
+    except ModuleNotFoundError as e:
+        tb = traceback.format_exc().strip().splitlines()
         module = tb[-1].split()[-1]
         path = os.path.dirname(sys.argv[0])
         full_path = f'{os.path.abspath(path)}/{sys.argv[0]}'
@@ -18,6 +22,7 @@ else:
         randline = random.randint(1, imports + 1)
         tb[1] = f'  File "{full_path}", line {randline}, in <module>'
         tb[2] = f'  import {module}'
+        tb[3] = f'ModuleNotFoundError: No module named \'{module}\''
         for line in tb:
             print(line, file=sys.stderr)
         sys.exit()
