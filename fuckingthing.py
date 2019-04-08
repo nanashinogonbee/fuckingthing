991b1b50363249442d9151430636951fbf535fbe
nanashinogonbee <vgpro80@gmail.com>
2019-04-08 06:13:47 +0300

    Crash position fixed

--- fuckingthing.py
+++ fuckingthing.py
@@ -18,9 +18,11 @@ else:
         path = os.path.dirname(sys.argv[0])
         full_path = f'{os.path.abspath(path)}/{sys.argv[0]}'
         script = open(full_path).readlines()
-        imports = len([i for i in script if i.startswith('import ')])
-        randline = random.randint(1, imports + 1)
-        tb[1] = f'  File "{full_path}", line {randline}, in <module>'
+        imports = [i for i in script if i.startswith('import ')]
+        imports = [' '.join(i.split()[1:]) for i in imports]
+        import_lines = [i + 1 for i in range(len(script)) if script[i].startswith('import ')]
+        err_line = import_lines[imports.index(module)]
+        tb[1] = f'  File "{full_path}", line {err_line}, in <module>'
         tb[2] = f'  import {module}'
         tb[3] = f'ModuleNotFoundError: No module named \'{module}\''
         for line in tb:
